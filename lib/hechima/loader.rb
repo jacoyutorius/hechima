78372ad887a40b0d852ade418f62845522ac2408
Yuto Ogi <jacoyutorius@gmail.com>
2015-02-23 17:40:29 +0900

    fix routing error issue

--- lib/hechima/loader.rb
+++ lib/hechima/loader.rb
@@ -1,12 +1,12 @@
 require "hechima/file"
 
 module Hechima
-	def self.load routing, views_dir="/app/views"
+	def self.load routing, hechima_root="/app/views"
 	  
-	  target_dir = File.join(Rails.root.to_s, views_dir)
+	  target_dir = File.join(Rails.root.to_s, hechima_root)
 	  anonymous_views = get_no_controller_views target_dir
 	  exclude_views = get_has_controller_views
-
+	  file_root = hechima_root.gsub(/\/app\/views/, "")
 
 	  (anonymous_views - exclude_views).each do |path|
 
@@ -15,10 +15,9 @@ module Hechima
 	      path_array.delete(path_array.last) if %w(index).include? path_array.last
 	      route = path_array.join "/"
 
-      	routing.get "#{route}" => "hechima#index", :defaults => {:file => "#{path}"}
+	      routing.get "#{route}" => "hechima#index", :defaults => {:file => "#{file_root}/#{path}"}
 	    else
-	      
-      	routing.get "#{path}" => "hechima#index", :defaults => {:file => "#{path}"}
+	      routing.get "#{path}" => "hechima#index", :defaults => {:file => "#{file_root}/#{path}"}
 	    end
 
 	  end
